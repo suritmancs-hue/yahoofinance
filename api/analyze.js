93129a8b3791ffa0b406e7c4c6ab45444e39cb2e
suritmancs-hue <suritmancs@gmail.com>
2025-12-12 00:03:09 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -64,8 +64,6 @@ async function processSingleTicker(ticker, interval, range) {
         const latestCandle = historyData[historyData.length - 1];
         let volSpikeRatio = 0;
         let volatilityRatio = 0;
-        let maxVol = 0;
-        let cekVol = 0;
         const PERIOD = 25;
 
         if (historyData.length > PERIOD) {
@@ -82,7 +80,7 @@ async function processSingleTicker(ticker, interval, range) {
             const maxPrevVolume = Math.max(...historicalOnly);
             const ratioVsMax = currentVolume / maxPrevVolume;
             
-            if (ratioVsMax >= 3) {
+            if (ratioVsMax >= 2) {
               volSpikeRatio = calculateVolumeRatio(currentVolume, maVolume);
             } else {
               volSpikeRatio = 0;
