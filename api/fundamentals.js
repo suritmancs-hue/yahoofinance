2cac2e5842da54156371360a6ffdad11c974cd03
suritmancs-hue <suritmancs@gmail.com>
2026-01-10 00:05:40 +0800

    Update fundamentals.js

--- api/fundamentals.js
+++ api/fundamentals.js
@@ -40,6 +40,7 @@ async function fetchFundamentalData(ticker) {
         const result = await yahooFinance.quoteSummary(symbol, {
             modules: ['defaultKeyStatistics', 'summaryDetail', 'price']
         });
+        console.log(result);
 
         if (!result) {
             return { ticker: symbol, status: "Not Found", note: "No Data" };
